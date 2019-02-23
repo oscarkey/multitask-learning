877d57f60e27f0dd8aa75e461f7bff11f75c04b8
Joanna Materzynska <joanna.materzynska@gmail.com>
2019-02-23 13:49:49 +0000

    adding script for resizing the dataset

---
+++ scripts/create_tiny_cityscapes.py
@@ -0,0 +1,28 @@
+import os
+from tqdm import tqdm
+from scipy.misc import imread, imsave, imresize
+import argparse
+
+def save_resized_file(output_folder, file_path, imsize=[128, 256]):
+    resized_image = imresize(imread(file_path), imsize)
+    new_path = os.path.join(output_folder,
+                            file_path.split('/')[-1])
+    imsave(new_path, resized_image)
+
+def main(root_folder, output_folder):
+    # get directories of the cities
+    cities = os.listdir(root_folder)
+    for city in cities:
+        temp_files = os.listdir(os.path.join(root_folder, city))
+        for file_ in tqdm(temp_files):
+            save_resized_file(os.path.join(root_folder, city, file_))
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-output_folder', type=str)
+    parser.add_argument('-height', type=int)
+    parser.add_argument('-width', type=int)
+    parser.add_argument('-root_folder', type=str)
+    args = parser.parse_args()
+    print(args)
