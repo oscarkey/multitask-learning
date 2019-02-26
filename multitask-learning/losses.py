6e49751d870739cba984f39fecc14548c6d2ed1c
Joanna Materzynska <joanna.materzynska@gmail.com>
2019-02-26 14:43:11 +0000

    adding total loss conditioning

--- multitask-learning/losses.py
+++ multitask-learning/losses.py
@@ -3,9 +3,11 @@ from torch import nn
 
 class MultiTaskLoss(nn.Module):
 
-    def __init__(self):
+    def __init__(self, loss_type, loss_weights):
         self.l1_loss = nn.L1Loss(size_average=True)
         self.cross_entropy = nn.CrossEntropyLoss()
+        self.loss_weighting_parameter = loss_weighting_parameter
+        self.loss_weights = loss_weights
 
     def semantic_segmentation_loss(self, semseg_input, semseg_target):
         return self.cross_entropy(semseg_input, semseg_target)
@@ -18,6 +20,14 @@ class MultiTaskLoss(nn.Module):
     def depth_loss(self):
         pass
 
+    def calculate_total_loss(self, *losses):
+        if self.loss_type == 'fixed'
+            total_loss = sum([losses[i] * self.loss_weights[i] for i in zip(losses, self.loss_weights)])
+        elif self.loss_type == 'learned':
+            raise NotImplementedError
+        else:
+            raise ValueError
+
     def forward(self, input, *target):
         semseg_input, instance_input, depth_input = input
         # MISSING depth target
@@ -25,5 +35,5 @@ class MultiTaskLoss(nn.Module):
         semseg_loss = self.semantic_segmentation_loss(semseg_input, semseg_target)
         instanceseg_loss = self.instance_segmentation_loss(instance_input, instance_target, instance_mask)
         depth_loss = self.depth_loss(depth_input, depth_target)
-        total_loss = None # weight uncertanity
+        total_loss = self.calculate_total_loss(semseg_loss, instanceseg_loss, depth_loss)
         return total_loss
