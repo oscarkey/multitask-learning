1f702cd70976ec3c979b62f49b9f186726c45658
Oscar Key <oscar.t.key@gmail.com>
2019-02-26 14:56:16 +0000

    Fix bugs and add comments to loss functions.

--- multitask-learning/losses.py
+++ multitask-learning/losses.py
@@ -1,13 +1,34 @@
+"""Contains the loss functions."""
+
 import torch
 from torch import nn
 
-class MultiTaskLoss(nn.Module):
 
-    def __init__(self, loss_type, loss_weights):
+class MultiTaskLoss(nn.Module):
+    """Computes and combines the losses for the three tasks.
+
+    Has two modes:
+    1) 'fixed': the losses multiplied by fixed weights and summed
+    2) 'learned': we learn the losses, not implemented...
+    """
+
+    def __init__(self, loss_type, loss_weights=None):
+        """Creates a new instance.
+
+        :param loss_type Either 'fixed' or 'learned'
+        :param loss_weights If 'fixed' then a 3 tuple (semantic seg weight, instance seg weight,
+        depth weight), if 'learned' then None.
+        """
+        super().__init__()
+
+        assert ((loss_type == 'learned' and loss_weights is None)
+                or loss_type == 'fixed' and len(loss_weights) == 3)
+
+        self.loss_type = loss_type
+        self.loss_weights = loss_weights
+
         self.l1_loss = nn.L1Loss(size_average=True)
         self.cross_entropy = nn.CrossEntropyLoss()
-        self.loss_weighting_parameter = loss_weighting_parameter
-        self.loss_weights = loss_weights
 
     def semantic_segmentation_loss(self, semseg_input, semseg_target):
         return self.cross_entropy(semseg_input, semseg_target)
@@ -15,25 +36,30 @@ class MultiTaskLoss(nn.Module):
     def instance_segmentation_loss(self, instance_input, instance_target, instance_mask):
         masked_input = torch.masked_selected(instance_input, instance_mask)
         masked_target = torch.masked_selected(instance_target, instance_mask)
-        return self.l1_loss(masked_input, masked_target) 
+        return self.l1_loss(masked_input, masked_target)
 
     def depth_loss(self):
-        pass
+        return 0
 
     def calculate_total_loss(self, *losses):
-        if self.loss_type == 'fixed'
-            total_loss = sum([losses[i] * self.loss_weights[i] for i in zip(losses, self.loss_weights)])
+        if self.loss_type == 'fixed':
+            return sum([loss * weight for loss, weight in zip(losses, self.loss_weights)])
+
         elif self.loss_type == 'learned':
             raise NotImplementedError
+
         else:
             raise ValueError
 
-    def forward(self, input, *target):
-        semseg_input, instance_input, depth_input = input
+    def forward(self, predicted, target):
+        semseg_pred, instance_pred, depth_pred = predicted
         # MISSING depth target
         semseg_target, instance_target, instance_mask = target
-        semseg_loss = self.semantic_segmentation_loss(semseg_input, semseg_target)
-        instanceseg_loss = self.instance_segmentation_loss(instance_input, instance_target, instance_mask)
-        depth_loss = self.depth_loss(depth_input, depth_target)
-        total_loss = self.calculate_total_loss(semseg_loss, instanceseg_loss, depth_loss)
-        return total_loss
+
+        semseg_loss = self.semantic_segmentation_loss(semseg_pred, semseg_target)
+        instanceseg_loss = self.instance_segmentation_loss(instance_pred,
+                                                           instance_target,
+                                                           instance_mask)
+        depth_loss = self.depth_loss(depth_pred, 0)
+
+        return self.calculate_total_loss(semseg_loss, instanceseg_loss, depth_loss)
