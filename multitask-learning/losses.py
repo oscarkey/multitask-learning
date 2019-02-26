848beb18b82cde9c2a63acf066278a2e671a5462
Joanna Materzynska <joanna.materzynska@gmail.com>
2019-02-26 14:34:34 +0000

    adding instance loss

--- multitask-learning/losses.py
+++ multitask-learning/losses.py
@@ -1,22 +1,29 @@
+import torch
 from torch import nn
 
 class MultiTaskLoss(nn.Module):
 
     def __init__(self):
-        pass
+        self.l1_loss = nn.L1Loss(size_average=True)
+        self.cross_entropy = nn.CrossEntropyLoss()
 
-    def semantic_segmentation_loss(self):
-        return nn.CrossEntropyLoss()
+    def semantic_segmentation_loss(self, semseg_input, semseg_target):
+        return self.cross_entropy(semseg_input, semseg_target)
 
-    def instance_segmentation_loss(self):
-        pass
+    def instance_segmentation_loss(self, instance_input, instance_target, instance_mask):
+        masked_input = torch.masked_selected(instance_input, instance_mask)
+        masked_target = torch.masked_selected(instance_target, instance_mask)
+        return self.l1_loss(masked_input, masked_target) 
 
     def depth_loss(self):
         pass
 
     def forward(self, input, *target):
-        semseg_loss = self.semantic_segmentation_loss(input[0], target[0])
-        instanceseg_loss = self.instance_segmentation_loss(input[1], target[1])
-        depth_loss = self.depth_loss(input[2], target[2])
+        semseg_input, instance_input, depth_input = input
+        # MISSING depth target
+        semseg_target, instance_target, instance_mask = target
+        semseg_loss = self.semantic_segmentation_loss(semseg_input, semseg_target)
+        instanceseg_loss = self.instance_segmentation_loss(instance_input, instance_target, instance_mask)
+        depth_loss = self.depth_loss(depth_input, depth_target)
         total_loss = None # weight uncertanity
         return total_loss
