39e2db9506c05e63f85176aac414e60c908854bd
Joanna Materzynska <joanna.materzynska@gmail.com>
2019-02-27 16:28:56 +0000

    removing capture function

--- multitask-learning/main.py
+++ multitask-learning/main.py
@@ -14,11 +14,8 @@ def config():
     height = 128 #TODO: pass through to model
     width = 256 #TODO: pass through to model
 
-@ex.capture
-def train_model(root_dir, max_iter, num_classes, batch_size):
-    train.train(root_dir, max_iter, num_classes, batch_size)
 
 @ex.automain
 def main(_run):
     # TODO: train, then test or whatever
-    train_model(_run)
+    train.train(_run)
