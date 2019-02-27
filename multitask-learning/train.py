a61aab8fd5a8e2c5fae42844cfa994140486700d
Joanna Materzynska <joanna.materzynska@gmail.com>
2019-02-27 16:50:34 +0000

    add learning rate scheduler

--- multitask-learning/train.py
+++ multitask-learning/train.py
@@ -29,12 +29,16 @@ def train(_run):
 
     initial_learning_rate = 2.5e-3
 
+    optimizer = torch.optim.SGD(learner.parameters(), lr=initial_learning_rate, momentum=0.9, nesterov=True, weight_decay=1e4)
+    lr_lambda = lambda x: initial_learning_rate * (1 - epoch / _run.config['max_iter']) ** 0.9
+    lr_scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda, last_epoch=_run.config['max_iter'])
+
+
+
     for epoch in range(_run.config['max_iter']):  # loop over the dataset multiple times
 
         #polynomial learning rate decay
-        learning_rate = initial_learning_rate*(1-epoch/_run.config['max_iter'])**0.9
-
-        optimizer = torch.optim.SGD(learner.parameters(), lr=learning_rate, momentum=0.9, nesterov=True, weight_decay=1e4)
+        lr_scheduler.step()
 
         running_loss = 0.0
         for i, data in enumerate(loader, 0):
