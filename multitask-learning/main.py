e82da3a372ca29f21a6b295d13f720996d9a5f57
Oscar Key <oscar.t.key@gmail.com>
2019-02-27 17:34:53 +0000

    Format things.

--- multitask-learning/main.py
+++ multitask-learning/main.py
@@ -1,6 +1,6 @@
 """Contains config and Sacred main entry point."""
-from sacred import Experiment
 import train
+from sacred import Experiment
 from sacred.observers import MongoObserver
 
 ex = Experiment()
@@ -11,15 +11,16 @@ mongo_observer = MongoObserver.create(
 )
 ex.observers.append(mongo_observer)
 
+
 @ex.config
 def config():
     """Contains the default config values."""
     batch_size = 8
     max_iter = 1000
-    root_dir='tiny_cityscapes_train'
-    num_classes=20
-    height = 128 #TODO: pass through to model
-    width = 256 #TODO: pass through to model
+    root_dir = 'tiny_cityscapes_train'
+    num_classes = 20
+    height = 128  # TODO: pass through to model
+    width = 256  # TODO: pass through to model
     loss_type = 'fixed'
     loss_weights = (1, 0, 0)
 
--- multitask-learning/train.py
+++ multitask-learning/train.py
@@ -1,14 +1,10 @@
+import cityscapes
+import torch
+import torch.nn as nn
 from decoders import Decoders
 from encoder import Encoder
 from losses import MultiTaskLoss
 
-import torch
-import torch.nn as nn
-import cityscapes 
-
-import torchvision.transforms as transforms
-
-from PIL import Image
 
 class MultitaskLearner(nn.Module):
     def __init__(self, num_classes):
@@ -21,7 +17,6 @@ class MultitaskLearner(nn.Module):
 
 
 def train(_run):
-
     loader = cityscapes.get_loader_from_dir(_run.config['root_dir'], _run.config)
     learner = MultitaskLearner(_run.config['num_classes'])
 
@@ -29,15 +24,17 @@ def train(_run):
 
     initial_learning_rate = 2.5e-3
 
-    optimizer = torch.optim.SGD(learner.parameters(), lr=initial_learning_rate, momentum=0.9, nesterov=True, weight_decay=1e4)
+    optimizer = torch.optim.SGD(learner.parameters(),
+                                lr=initial_learning_rate,
+                                momentum=0.9,
+                                nesterov=True,
+                                weight_decay=1e4)
     lr_lambda = lambda x: initial_learning_rate * (1 - x / _run.config['max_iter']) ** 0.9
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
 
-
-
     for epoch in range(_run.config['max_iter']):  # loop over the dataset multiple times
 
-        #polynomial learning rate decay
+        # polynomial learning rate decay
         lr_scheduler.step()
 
         running_loss = 0.0
@@ -50,13 +47,13 @@ def train(_run):
             # forward + backward + optimize
             output_semantic, output_instance, output_depth = learner(inputs.float())
             loss = criterion((output_semantic, output_instance, output_depth),
-                              semantic_labels.long(), instance_centroid, instance_mask)
+                             semantic_labels.long(), instance_centroid, instance_mask)
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
-            #if i % 2000 == 1999:    # print every 2000 mini-batches
+            # if i % 2000 == 1999:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss))
             running_loss = 0.0
