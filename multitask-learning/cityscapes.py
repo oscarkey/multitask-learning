6dfa08b8718462938120a2301d7c244e83931d2e
Oscar Key <oscar.t.key@gmail.com>
2019-03-13 14:09:57 +0000

    Add random cropping transform to dataloader.

--- multitask-learning/cityscapes.py
+++ multitask-learning/cityscapes.py
@@ -9,6 +9,36 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 
+class NoopTransform(object):
+    """A transform that returns the original image unmodified."""
+
+    def __call__(self, image):
+        return image
+
+
+class RandomCrop(object):
+    """Crop randomly the image in a sample.
+
+    Taken from: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+
+    :arg output_size (width, height) tuple
+    """
+
+    def __init__(self, output_size: (int, int)):
+        assert isinstance(output_size, tuple)
+        assert len(output_size) == 2
+        self.output_size = output_size
+
+    def __call__(self, image):
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        return image[top: top + new_h, left: left + new_w]
+
+
 class CityscapesDataset(Dataset):
     """A Dataset which loads the Cityscapes dataset from disk.
 
@@ -20,9 +50,9 @@ class CityscapesDataset(Dataset):
     this class for each (though maybe we want different splits...).
     """
 
-    def __init__(self, root_dir: str):
-        # TODO: Potentially add transform for resizing data.
+    def __init__(self, root_dir: str, transform=NoopTransform()):
         self._root_dir = root_dir
+        self._transform = transform
         self._file_prefixes = self._find_file_prefixes(root_dir)
 
     @staticmethod
@@ -55,28 +85,32 @@ class CityscapesDataset(Dataset):
         return os.path.join(directory, prefix)
 
     def __getitem__(self, index: int):
-        image_file = self._get_file_path_for_index(index, 'leftImg8bit')
-        label_file = self._get_file_path_for_index(index, 'labelIds')
-
-        image_array = np.asarray(Image.open(image_file), dtype=np.float32)
-        # Rescale the image from [0,255] to [0,1].
-        image_array = image_array / 255 * 2 - 1
-        assert len(image_array.shape) == 3, 'image_array should have 3 dimensions'+ image_file
-        label_array = np.asarray(Image.open(label_file), dtype=np.int64)
-        assert len(label_array.shape) == 2, 'label_array should have 2 dimensions'+ label_file
-        instance_file = self._get_file_path_for_index(index, 'instanceIds')
-        instance_image = Image.open(instance_file)
-        instance_array = np.asarray(instance_image, dtype=np.float32)
-        assert len(instance_array.shape) == 2, 'instance_array should have 2 dimensions'+ instance_file
-        instance_vecs, instance_mask = self._compute_centroid_vectors(instance_array)
-
         # We load the images as H x W x channel, but we need channel x H x W.
         axis_order = (2, 0, 1)
 
-        return (np.transpose(image_array, axis_order),
-                label_array,
-                np.transpose(instance_vecs, axis_order),
-                instance_mask)
+        image_file = self._get_file_path_for_index(index, 'leftImg8bit')
+        image_array = np.asarray(Image.open(image_file), dtype=np.float32)
+        image_array = self._transform(image_array)
+        # Rescale the image from [0,255] to [0,1].
+        image_array = image_array / 255 * 2 - 1
+        image_array = np.transpose(image_array, axis_order)
+        assert len(image_array.shape) == 3, 'image_array should have 3 dimensions' + image_file
+
+        label_file = self._get_file_path_for_index(index, 'labelIds')
+        label_array = np.asarray(Image.open(label_file), dtype=np.int64)
+        label_array = self._transform(label_array)
+        assert len(label_array.shape) == 2, 'label_array should have 2 dimensions' + label_file
+
+        instance_file = self._get_file_path_for_index(index, 'instanceIds')
+        instance_array = np.asarray(Image.open(instance_file), dtype=np.float32)
+        assert len(instance_array.shape) == 2, 'instance_array should have 2 dimensions' + instance_file
+        instance_vecs, instance_mask = self._compute_centroid_vectors(instance_array)
+        instance_vecs = self._transform(instance_vecs)
+        instance_mask = self._transform(instance_mask)
+        # We don't need to transpose the mask as it has no channels.
+        instance_vecs = np.transpose(instance_vecs, axis_order)
+
+        return image_array, label_array, instance_vecs, instance_mask
 
     def _get_file_path_for_index(self, index: int, type: str) -> str:
         path_prefix = self._file_prefixes[index]
@@ -119,7 +153,7 @@ class CityscapesDataset(Dataset):
             mask = np.asarray(mask.mask, dtype=np.uint8)
         else:
             assert mask.mask == False, 'mask is all True'
-            mask = np.zeros((128,256), dtype=np.uint8)
+            mask = np.zeros((128, 256), dtype=np.uint8)
         mask = np.stack((mask, mask))
         return vecs, mask
 
@@ -127,13 +161,13 @@ class CityscapesDataset(Dataset):
         return len(self._file_prefixes)
 
 
-def get_loader_from_dir(root_dir: str, config):
+def get_loader_from_dir(root_dir: str, config, transform=NoopTransform()):
     """Creates a DataLoader for Cityscapes from the given root directory.
 
     Will load any data file in any sub directory under the root directory.
     """
 
-    return get_loader(CityscapesDataset(root_dir), config)
+    return get_loader(CityscapesDataset(root_dir, transform=transform), config)
 
 
 def get_loader(dataset: Dataset, config):
