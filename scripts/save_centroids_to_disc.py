18d873d9a9ef1762935a724e53857afd7e0f7867
Oscar Key <oscar.t.key@gmail.com>
2019-03-20 20:29:33 +0000

    Format.

--- scripts/save_centroids_to_disc.py
+++ scripts/save_centroids_to_disc.py
@@ -1,8 +1,10 @@
-import os
 import argparse
+import os
+
+import numpy as np
 from PIL import Image
 from tqdm import tqdm
-import numpy as np
+
 
 def _compute_centroid_vectors(instance_image):
     """For each pixel, calculate the vector from that pixel to the centre of its instance.
@@ -48,10 +50,8 @@ def save_mask_instances_file(output_folder, file_path):
     instance_array = np.asarray(Image.open(file_path), dtype=np.float32)
     file_path = file_path.replace('instanceIds', 'instanceMask')
     instance_vecs, instance_mask = _compute_centroid_vectors(instance_array)
-    new_path = os.path.join(output_folder,
-                            file_path.split('/')[-1])
-    instance = {"mask": instance_mask,
-                "vec": instance_vecs}
+    new_path = os.path.join(output_folder, file_path.split('/')[-1])
+    instance = {"mask": instance_mask, "vec": instance_vecs}
     np.save(new_path, instance)
 
 
@@ -74,12 +74,11 @@ def main(root_folder, output_folder):
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     parser.add_argument('root_folder')
     # parser.add_argument('output_folder')
-    #parser.add_argument('-height', type=int)
-    #parser.add_argument('-width', type=int)
+    # parser.add_argument('-height', type=int)
+    # parser.add_argument('-width', type=int)
     args = vars(parser.parse_args())
 
     main(args['root_folder'], args['root_folder'])
